4a55fa021706e7345e31c4ffdea575498c379def
bugfact@gmail.com <bugfact@gmail.com@1df4be1c-db3d-b7c2-9e46-d9c5b5f69acc>
2011-12-16 08:55:54 +0000

    Added serialization code and test project

---
+++ trunk/DogFightTests/SerializationTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using DogFight;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DogFightTests
+{
+    [TestClass]
+    public class SerializationTests
+    {
+        [TestMethod]
+        public void TestSerialization()
+        {
+            var world = new World();
+
+            var spitfire = new Fighter {Name = "Spitfire", PositionX = 100, PositionY = 200, Rotation = 45};
+            var stuka = new Fighter{Name = "Stuka", PositionX = 300, PositionY = 400, Rotation = 90, Target = spitfire};
+
+            world.Fighters.Add(spitfire);
+            world.Fighters.Add(stuka);
+
+            var stream = new MemoryStream();
+            world.SaveTo(stream);
+            Assert.IsTrue(stream.Length > 0 );
+
+            // Deserialize and check result.
+            stream.Position = 0;
+
+            world = World.LoadFrom(stream);
+
+            Assert.IsNotNull(world.Fighters);
+            Assert.AreEqual(world.Fighters.Count, 2);
+
+            spitfire = world.Fighters.FirstOrDefault(f => f.Name == "Spitfire");
+            Assert.IsNotNull(spitfire);
+            Assert.AreEqual(spitfire.Name, "Spitfire");
+            Assert.AreEqual(spitfire.PositionX, 100);
+            Assert.AreEqual(spitfire.PositionY, 200);
+            Assert.AreEqual(spitfire.Rotation, 45);
+            
+            stuka = world.Fighters.FirstOrDefault(f => f.Name == "Stuka");
+            Assert.IsNotNull(stuka);
+            Assert.AreEqual(stuka.Name, "Stuka");
+            Assert.AreEqual(stuka.PositionX, 300);
+            Assert.AreEqual(stuka.PositionY, 400);
+            Assert.AreEqual(stuka.Rotation, 90);
+            Assert.AreEqual(stuka.Target, spitfire);
+        }
+    }
+}
