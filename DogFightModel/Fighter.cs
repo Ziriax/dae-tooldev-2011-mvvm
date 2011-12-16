9a18f4af3435f27268a122aca7f3a34bb009a7c1
bugfact <bugfact@gmail.com>
2011-12-16 08:39:06 +0000

    Added DataContract serialization attributes

--- DogFightModel/Fighter.cs
+++ DogFightModel/Fighter.cs
@@ -1,11 +1,23 @@
-﻿namespace DogFight
+﻿using System.Runtime.Serialization;
+
+namespace DogFight
 {
+    [DataContract]
     public class Fighter
     {
+        [DataMember]
         public string Name;
+
+        [DataMember]
         public double PositionX;
+
+        [DataMember]
         public double PositionY;
+
+        [DataMember]
         public double Rotation;
+
+        [DataMember]
         public Fighter Target;
     }
 }
--- DogFightModel/World.cs
+++ DogFightModel/World.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace DogFight
 {
+    [DataContract]
     public class World
     {
+        [DataMember]
         public readonly List<Fighter> Fighters = new List<Fighter>();
     }
 }
