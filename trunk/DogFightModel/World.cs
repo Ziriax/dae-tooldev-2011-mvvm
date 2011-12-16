d17bdd59c16d66e7aad497c2f820f1f0ef23e485
bugfact@gmail.com <bugfact@gmail.com@1df4be1c-db3d-b7c2-9e46-d9c5b5f69acc>
2011-12-16 08:43:41 +0000

    Added save and load methods to world

--- trunk/DogFightModel/World.cs
+++ trunk/DogFightModel/World.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Xml;
 
 namespace DogFight
 {
@@ -12,5 +13,51 @@ namespace DogFight
     {
         [DataMember]
         public readonly List<Fighter> Fighters = new List<Fighter>();
+
+        private static DataContractSerializer CreateSerializer()
+        {
+            var serializer = new DataContractSerializer(
+                typeof(World),
+                new[] { typeof(Fighter) },
+                int.MaxValue,
+                true,
+                true,
+                null);
+
+            return serializer;
+        }
+
+        public void SaveTo(Stream stream)
+        {
+            var serializer = CreateSerializer();
+
+            var xmlSettings = new XmlWriterSettings
+                {
+                    CloseOutput = false,
+                    Indent = true,
+                    NewLineChars = Environment.NewLine,
+                };
+
+            using( var xmlWriter = XmlWriter.Create(stream, xmlSettings) )
+            {
+                serializer.WriteObject(xmlWriter, this);
+            }
+        }
+
+        public static World LoadFrom(Stream stream)
+        {
+            var serializer = CreateSerializer();
+
+            var xmlSettings = new XmlReaderSettings
+            {
+                CloseInput = false,
+            };
+
+            using (var xmlReader = XmlReader.Create(stream, xmlSettings))
+            {
+                var deserializedObject = serializer.ReadObject(xmlReader);
+                return (World) deserializedObject;
+            }
+        }
     }
 }
